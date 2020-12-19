7409f655f6ed88a5ecc1896dc3dca732c04a7358
D3 <70775520+D3CRYPT360@users.noreply.github.com>
2020-12-19 15:43:09 +0500

    bumped version to 11.6

--- extras/help.py
+++ extras/help.py
@@ -6,7 +6,7 @@ import asyncio
 class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.bot.version = "10.6"
+        self.bot.version = "11.0"
 
     @commands.command()
     async def help(self, ctx):
