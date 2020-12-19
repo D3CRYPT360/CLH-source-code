ad3b4f7fb050b8430f4ac0c9e9a55e57065eea55
KeroQuero <keremcarikcilar@gmail.com>
2020-12-19 13:20:21 +0300

    made a lot of changes

--- extras/help.py
+++ extras/help.py
@@ -2,8 +2,6 @@ import discord
 from discord.ext import commands
 from disputils import BotEmbedPaginator
 import asyncio
-import sys, traceback
-import platform
 
 class Help(commands.Cog):
     def __init__(self, bot):
