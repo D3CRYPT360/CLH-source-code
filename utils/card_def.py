0274565a881593ef4c432f2f75bdc356ca3215b0
D3crypt360 <nahilali3600@gmail.com>
2020-12-29 01:22:17 +0500

    fixed brimstone card bug

--- utils/card_def.py
+++ utils/card_def.py
@@ -1,107 +1,107 @@
 def card_coverter_global(card):
-    if card == "Brimstone" or "Brim" or "Sarge":
+    if card == "Brimstone":
         return "No One Left Behind Card"
     
-    elif card == "Viper" or "Pandemic" or "Sabine":
+    elif card == "Viper":
         return "One Dark Night Card"
     
-    elif card == "Omen" or "Wraith":
+    elif card == "Omen":
         return "What's Another Death Card"
     
-    elif card == "Killjoy" or "Kj":
+    elif card == "Killjoy":
         return "Nächtelang Card"
     
-    elif card == "Cypher" or "Aamir":
+    elif card == "Cypher":
         return "Redeemers Folly Card"
     
-    elif card == "Sova" or "Hunter" or "Constant" or "Constantvalorant":
+    elif card == "Sova":
         return "Through The Looking Glass Card"
     
-    elif card == "Sage" or "Thorne":
+    elif card == "Sage":
         return "Journey Of Trials Card"
     
-    elif card == "Phoenix" or "Phx":
+    elif card == "Phoenix":
         return "Firestarter Card"
     
-    elif card == "Jett" or "Wushu":
+    elif card == "Jett":
         return "Mirrored Edge Card"
     
-    elif card == "Reyna" or "Vampire":
+    elif card == "Reyna":
         return "Revenge For Life Card"
 
-    elif card == "Raze" or "Clay":
+    elif card == "Raze":
         return "Rising Up Card"
     
-    elif card == "Breach" or "Cabbage" or "Lettuce":
+    elif card == "Breach":
         return "Big Payday Card"
     
-    elif card == "Skye" or "Guide":
+    elif card == "Skye":
         return "The Great Reclaimer Card"
     
 def card_coverter_beta(card):
-    if card == "Brimstone" or "Brim" or "Sarge":
+    if card == "Brimstone":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750327062505717791/Coffee.png",
             "https://cdn.discordapp.com/attachments/747483388415311883/750327090166890526/Open_Up_The_Sky.png"
             ]
         return page_list
     
-    elif card == "Viper" or "Pandemic" or "Sabine":
+    elif card == "Viper":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750328266208051260/Viper_Lab_Coat.png",
             "https://cdn.discordapp.com/attachments/747483388415311883/750328404380876901/Viper_Ult.webp"
             ]
         return page_list
     
-    elif card == "Omen" or "Wraith":
+    elif card == "Omen":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750327950020313088/Omen_Teleport.png",
             "https://cdn.discordapp.com/attachments/747483388415311883/750328152936677376/Omen_Ult.webp"
             ]
         return page_list
 
-    elif card == "Cypher" or "Aamir":
+    elif card == "Cypher":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750327597644382298/Cypher_1_m1.png",
             "https://cdn.discordapp.com/attachments/747483388415311883/750327609199689808/Cypher_card.png"
             ]
         return page_list
     
-    elif card == "Sova" or "Hunter" or "Constant" or "Constantvalorant":
+    elif card == "Sova":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750327862162227210/Sova_Pose.png",
             "https://cdn.discordapp.com/attachments/747483388415311883/750327874069725244/Sova_Ult.png"
             ]
         return page_list
     
-    elif card == "Sage" or "Thorne":
+    elif card == "Sage":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750326961708204092/Balanced.png",
             "https://cdn.discordapp.com/attachments/747483388415311883/750326973586472970/Harmony.png"
             ]
         return page_list
     
-    elif card == "Phoenix" or "Phx":
+    elif card == "Phoenix":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750327686831800430/Phoenix_Sun.png",
             "https://cdn.discordapp.com/attachments/747483388415311883/750327701642018887/Phoenix_Down.png"
             ]
         return page_list
     
-    elif card == "Jett" or "Wushu":
+    elif card == "Jett":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750327767425351721/Jett_Card.webp",
             "https://cdn.discordapp.com/attachments/747483388415311883/750327779127459840/Jett_knife.jpg"
             ]
         return page_list
     
-    elif card == "Raze" or "Clay":
+    elif card == "Raze":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750328574078091395/Raze_Up.png"
             ]
         return page_list
     
-    elif card == "Breach" or "Cabbage" or "Lettuce":
+    elif card == "Breach":
         page_list = [
             "https://cdn.discordapp.com/attachments/747483388415311883/750327494321766411/Off_Your_Feet.png",
             "https://cdn.discordapp.com/attachments/747483388415311883/750327511837311076/Breach_2_dog.png"
