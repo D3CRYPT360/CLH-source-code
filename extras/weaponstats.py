61549c5d75204008a4983529d30648a9a876d151
D3crypt360 <nahilali3600@gmail.com>
2020-12-28 09:01:34 +0500

    Initial commit

---
+++ extras/weaponstats.py
@@ -0,0 +1,97 @@
+import discord
+from discord.ext import commands
+from utils.requesting import Weapon
+import DiscordUtils
+
+
+
+"""
+Str = ''
+For i in range(len(json_data[][]['damageRanges'])):
+    Range = json_data[][]['damageRanges'][I]
+    Str += f'range 
+{Range['rangestart]}-{range['rangeend]} is \n head {range['head']} /n leg {range['leg']}'
+
+"""
+
+
+# ("```py"+
+#         "\n Firerate: {}".format(json_data['data'][i]['weaponStats']['fireRate']) +
+#         "\n Magazine size: {}".format(json_data['data'][i]['weaponStats']['magazineSize']) +
+#         "\n First Bullet Accuracy: {}".format(json_data['data'][i]['weaponStats']['firstBulletAccuracy']) +
+#         "\n Wall Penetration: {}".format((json_data['data'][i]['weaponStats']['wallPenetration']).replace("EWallPenetrationDisplayType::", '')) +
+#         "\n------------------------" +
+#         "\n Damage done by {} without armour".format(json_data['data'][i]['displayName']) +
+#         "\n Head Damage: {}".format(json_data['data'][i]['weaponStats']['damageRanges'][0]['headDamage']) +
+#         "\n Body Damage: {}".format(json_data['data'][i]['weaponStats']['damageRanges'][0]['bodyDamage']) +
+#         "\n Leg Damage: {}".format(json_data['data'][i]['weaponStats']['damageRanges'][0]['legDamage']) +
+#         "\n------------------------" +
+#         "\n Damage ranges of {} with armour".format(json_data['data'][i]['displayName']) +
+#         "\n Head Damage: {}".format(json_data['data'][i]['weaponStats']['damageRanges'][1]['headDamage']) +
+#         "\n Body Damage: {}".format(json_data['data'][i]['weaponStats']['damageRanges'][1]['bodyDamage']) +
+#         "\n Leg Damage: {}```".format(json_data['data'][i]['weaponStats']['damageRanges'][1]['legDamage'])
+#         )
+                     
+
+class weaponStats(commands.Cog):
+    def __init__(self, bot):
+        self.bot=bot
+
+    @commands.command()
+    async def weapon(self, ctx, weapon):
+        json_data = Weapon()
+        weapon = weapon.capitalize()
+        Str = ''
+        for i in range(len(json_data['data'])):
+            if json_data['data'][i]['displayName'] == weapon:
+                for x in range(len(json_data['data'][i]['weaponStats']['damageRanges'])):
+                    Range = json_data['data'][i]['weaponStats']['damageRanges']
+                    Str += ("```py" +
+                            f"\n Damage from range {Range[x]['rangeStartMeters']}M to {Range[x]['rangeEndMeters']}M is "+
+                            "\n Head Damage: {}".format(Range[x]['headDamage']) +
+                            "\n Body Damage: {}".format(Range[x]['bodyDamage']) +
+                            "\n Leg Damage: {}```".format(Range[x]['bodyDamage'])
+                        )        
+                embed1 = discord.Embed(
+                    colour = discord.Color.dark_theme(),
+                    title = "__{}__".format(json_data['data'][i]['displayName'])
+                )
+                embed1.add_field(name="__{} Stats__".format(json_data['data'][i]['displayName']),
+                                value=(
+                                    "```py"+
+                                    "\n Firerate: {}".format(json_data['data'][i]['weaponStats']['fireRate']) +
+                                    "\n Magazine size: {}".format(json_data['data'][i]['weaponStats']['magazineSize']) +
+                                    "\n First Bullet Accuracy: {}".format(json_data['data'][i]['weaponStats']['firstBulletAccuracy']) +
+                                    "\n Wall Penetration: {}```".format((json_data['data'][i]['weaponStats']['wallPenetration']).replace("EWallPenetrationDisplayType::", '')))
+                                )
+                embed1.set_image(url="{}".format(json_data['data'][i]['displayIcon']))
+
+                embed2 = discord.Embed(
+                    colour = discord.Color.dark_theme(),
+                    title = "__{}__".format(json_data['data'][i]['displayName'])
+                )
+                embed2.add_field(name="__{} Damage from range chart__".format(json_data['data'][i]['displayName']),value= Str)
+                embed2.set_image(url="{}".format(json_data['data'][i]['displayIcon']))
+                
+                paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx, auto_footer = True)
+                paginator.add_reaction('⬅️', "back")
+                paginator.add_reaction('➡️', "next")
+                embeds = [embed1, embed2]
+                await paginator.run(embeds)
+
+"""
+            "rangeStartMeters": 30,
+            "rangeEndMeters": 50,damageRanges
+            "headDamage": 77.5,
+            "bodyDamage": 31,
+            "legDamage": 26.35
+          }
+        ]
+      },
+"""
+"""
+
+"""
+def setup(bot):
+    bot.add_cog(weaponStats(bot))
+    print("weaponstats.py loaded")
