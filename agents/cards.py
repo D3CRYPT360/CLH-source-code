8a59deec3e9327fd610e41faef0964deed047907
D3crypt360 <nahilali3600@gmail.com>
2020-12-29 19:03:45 +0500

    fixed Brimstone card bug

--- agents/cards.py
+++ agents/cards.py
@@ -13,12 +13,12 @@ class Cards(commands.Cog):
     @commands.command()
     async def card(self, ctx, card):
         json_data = Card()
-        hexa = card.capitalize()
-        card_name = card_coverter_global(hexa)
+        card = card.capitalize()
+        card_name = card_coverter_global(card)
         for i in range(len(json_data['data'])):
             if json_data['data'][i]['displayName'] == card_name:
                 embed = discord.Embed(
-                    colour = agent_hex(hexa),
+                    colour = agent_hex(card),
                     title='{}'.format(card_name)
                 )
                 embed.set_image(url="{}".format(json_data['data'][i]['largeArt']))
@@ -28,30 +28,34 @@ class Cards(commands.Cog):
     async def beta(self, ctx, beta_card):
         json_data = Agents()
         beta_card = beta_card.capitalize()
-        if beta_card == "Raze":
-            embed = discord.Embed(
-                colour = agent_hex(beta_card),
-            )
-            embed.set_image(url="{}".format(card_coverter_beta(beta_card)[0]))
-            await ctx.send(embed=embed)
-        else: 
-            for i in range(len(json_data['data'])):
-                if json_data['data'][i]['displayName'] == beta_card:
-                    embed1 = discord.Embed(
-                        colour = agent_hex(beta_card),
-                    )
-                    embed1.set_image(url="{}".format(card_coverter_beta(beta_card)[0]))
-                    
-                    embed2 = discord.Embed(
-                        colour = agent_hex(beta_card),
-                    )
-                    embed2.set_image(url="{}".format(card_coverter_beta(beta_card)[1]))
-                    
-                    paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx, auto_footer = True)
-                    paginator.add_reaction('⬅️', "back")
-                    paginator.add_reaction('➡️', "next")
-                    embeds = [embed1, embed2]
-                    await paginator.run(embeds)
+        try:
+            
+            if beta_card == "Raze":
+                embed = discord.Embed(
+                    colour = agent_hex(beta_card),
+                )
+                embed.set_image(url="{}".format(card_coverter_beta("Raze")))
+                await ctx.send(embed=embed)
+            else: 
+                for i in range(len(json_data['data'])):
+                    if json_data['data'][i]['displayName'] == beta_card:
+                        embed1 = discord.Embed(
+                            colour = agent_hex(beta_card),
+                        )
+                        embed1.set_image(url="{}".format(card_coverter_beta(beta_card)[0]))
+                        
+                        embed2 = discord.Embed(
+                            colour = agent_hex(beta_card),
+                        )
+                        embed2.set_image(url="{}".format(card_coverter_beta(beta_card)[1]))
+                        
+                        paginator = DiscordUtils.Pagination.CustomEmbedPaginator(ctx, auto_footer = True)
+                        paginator.add_reaction('⬅️', "back")
+                        paginator.add_reaction('➡️', "next")
+                        embeds = [embed1, embed2]
+                        await paginator.run(embeds)
+        except TypeError:
+            await ctx.send(f"{beta_card} was not an Agent in the closed Beta. Try again with an agent who was in the closed beta", delete_after=5)
 
 def setup(bot):
     bot.add_cog(Cards(bot))
