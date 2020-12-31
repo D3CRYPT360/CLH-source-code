8744474d7f26b66d30412631b185c6e543fdfa5a
KeroQuero <keremcarikcilar@gmail.com>
2020-12-31 18:09:00 +0300

    suggestions

--- agents/test.py
+++
@@ -1,6 +0,0 @@
-# from Utils.agent_def import real_name, agent_country, agent_order
-# # from utils.requesting import Agents
-
-# print(agent_country("Jett"))
-# print(real_name("Jett"))
-# print(agent_order("Jett"))
