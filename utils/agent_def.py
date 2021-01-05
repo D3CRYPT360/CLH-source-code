bed2bea0721bd793017a73b3bcbb5cea885fae7c
d3crypt360 <nahilali3600@gmail.com>
2021-01-05 09:42:45 +0500

    elif -> if

--- utils/agent_def.py
+++ utils/agent_def.py
@@ -4,200 +4,212 @@ def agent_country(fake_input):
     if fake_input == "Brimstone":
         return "United States of America"
     
-    elif fake_input == "Viper":
+    if fake_input == "Viper":
         return "United States of America"
     
-    elif fake_input == "Omen":
+    if fake_input == "Omen":
         return "Unknown"
         
-    elif fake_input == "Killjoy":
+    if fake_input == "Killjoy":
         return "Germany"
     
     if fake_input == "Cypher":
         return "Morocco"
     
-    elif fake_input == "Sova":
+    if fake_input == "Sova":
         return "Russia"
     
-    elif fake_input == "Sage":
+    if fake_input == "Sage":
         return "China"
     
-    elif fake_input == "Phoenix":
+    if fake_input == "Phoenix":
         return "United Kingdom"
     
-    elif fake_input == "Jett":
+    if fake_input == "Jett":
         return "South Korea"
     
-    elif fake_input == "Reyna":
+    if fake_input == "Reyna":
         return "Mexico"
 
-    elif fake_input == "Raze":
+    if fake_input == "Raze":
         return "Brazil"
     
-    elif fake_input == "Breach":
+    if fake_input == "Breach":
         return "Sweden"
     
-    elif fake_input == "Skye":
+    if fake_input == "Skye":
         return "Australia"
+
+    if fake_input == "Yoru":
+        return "Japan"
 
 def real_name(fake_input):
     if fake_input == "Brimstone":
         return "Unknown"
     
-    elif fake_input == "Viper":
+    if fake_input == "Viper":
         return "Sabine"
     
-    elif fake_input == "Omen":
+    if fake_input == "Omen":
         return "Unkown"
     
-    elif fake_input == "Killjoy":
+    if fake_input == "Killjoy":
         return "Unkown"
     
     if fake_input == "Cypher":
         return "Aamir"
     
-    elif fake_input == "Sova":
+    if fake_input == "Sova":
         return "Unknown"
     
-    elif fake_input == "Sage":
+    if fake_input == "Sage":
         return "Unknown"
     
-    elif fake_input == "Phoenix":
+    if fake_input == "Phoenix":
         return "Grant Galloway"
     
-    elif fake_input == "Jett":
+    if fake_input == "Jett":
         return "Joon Hee"
     
-    elif fake_input == "Reyna":
+    if fake_input == "Reyna":
         return "Unknown"
     
-    elif fake_input == "Raze":
+    if fake_input == "Raze":
         return "Unknown"
 
-    elif fake_input == "Breach":
+    if fake_input == "Breach":
         return "Unknown"
     
-    elif fake_input == "Skye":
+    if fake_input == "Skye":
         return "Emily"
+
+    if fake_input == "Yoru":
+        return "Unkown"
     
 def agent_order(fake_input):
     if fake_input == "Brimstone":
         return "1"
     
-    elif fake_input == "Viper":
+    if fake_input == "Viper":
         return "2"
     
-    elif fake_input == "Omen":
+    if fake_input == "Omen":
         return "3"
     
-    elif fake_input == "Killjoy" :
+    if fake_input == "Killjoy" :
         return "4"
     
-    elif fake_input == "Cypher":
+    if fake_input == "Cypher":
         return "5"
     
-    elif fake_input == "Sova":
+    if fake_input == "Sova":
         return "6"
     
-    elif fake_input == "Sage":
+    if fake_input == "Sage":
         return "7"
     
-    elif fake_input == "Phoenix":
+    if fake_input == "Phoenix":
         return "9"
     
-    elif fake_input == "Jett":
+    if fake_input == "Jett":
         return "10"
         
-    elif fake_input == "Reyna":
+    if fake_input == "Reyna":
         return "11"
     
-    elif fake_input == "Raze":
+    if fake_input == "Raze":
         return "12"
     
-    elif fake_input == "Breach":
+    if fake_input == "Breach":
         return "13"
     
-    elif fake_input == "Skye":
+    if fake_input == "Skye":
         return "14"
+
+    if fake_input == "Yoru":
+        return "15"
 
 def lore_link_official(fake_input):
     if fake_input == "Brimstone":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742455802035437568"
     
-    elif fake_input == "Viper":
+    if fake_input == "Viper":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742456639277236294"
     
-    elif fake_input == "Omen":
+    if fake_input == "Omen":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742457266254381186"
     
-    elif fake_input == "Killjoy":
+    if fake_input == "Killjoy":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742457949313695774"
     
-    elif fake_input == "Cypher":
+    if fake_input == "Cypher":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742696637708763138"
     
-    elif fake_input == "Sova":
+    if fake_input == "Sova":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742696788128956466"
     
-    elif fake_input == "Sage":
+    if fake_input == "Sage":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742697064365949030"
     
-    elif fake_input == "Phoenix":
+    if fake_input == "Phoenix":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742698018012266547"
     
-    elif fake_input == "Jett":
+    if fake_input == "Jett":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742698186686332929"
         
-    elif fake_input == "Reyna":
+    if fake_input == "Reyna":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742698335168888874"
     
-    elif fake_input == "Raze":
+    if fake_input == "Raze":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742698493680025611"
     
-    elif fake_input == "Breach":
+    if fake_input == "Breach":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/742698622101225644"
     
-    elif fake_input == "Skye":
+    if fake_input == "Skye":
         return "https://discordapp.com/channels/708983243847761931/727953100110823446/770698138972454932"
     
+    if fake_input == "Yoru":
+        return "https://discord.com/channels/708983243847761931/727811131254505523/795128590113964042"
+
 def lore_link_unofficial(fake_input):
     if fake_input == "Brimstone":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/748649585211211878"
     
-    elif fake_input == "Viper":
+    if fake_input == "Viper":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/748654881392296067"
     
-    elif fake_input == "Omen":
+    if fake_input == "Omen":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/748667838100668549"
     
-    elif fake_input == "Killjoy":
+    if fake_input == "Killjoy":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/748695653500059748"
     
-    elif fake_input == "Cypher":
+    if fake_input == "Cypher":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/748711251928154232"
     
-    elif fake_input == "Sova":
+    if fake_input == "Sova":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/748860067549413427"
     
-    elif fake_input == "Sage":
+    if fake_input == "Sage":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/748906193757405216"
     
-    elif fake_input == "Phoenix":
+    if fake_input == "Phoenix":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/748979255374774494"
     
-    elif fake_input == "Jett":
+    if fake_input == "Jett":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/748990254010204311"
         
-    elif fake_input == "Reyna":
+    if fake_input == "Reyna":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/749018798543339572"
     
-    elif fake_input == "Raze":
+    if fake_input == "Raze":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/749202008090869850"
-    
-    elif fake_input == "Breach":
+  
+    if fake_input == "Breach":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/749222091219402842"
     
-    elif fake_input == "Skye":
+    if fake_input == "Skye":
         return "https://discordapp.com/channels/708983243847761931/747484591836758127/763465241966542908"
     
     
@@ -205,38 +217,89 @@ def agent_hex(fake_input):
     if fake_input == "Brimstone":
         return 0xD1641B
     
-    elif fake_input == "Viper":
+    if fake_input == "Viper":
         return discord.Colour.green()
     
-    elif fake_input == "Omen":
+    if fake_input == "Omen":
         return 0x5856BD
     
-    elif fake_input == "Killjoy":
+    if fake_input == "Killjoy":
         return 0xffff00
     
-    elif fake_input == "Cypher":
+    if fake_input == "Cypher":
         return discord.Colour.dark_grey()
     
-    elif fake_input == "Sova":
+    if fake_input == "Sova":
         return discord.Colour.blue()
     
-    elif fake_input == "Sage":
+    if fake_input == "Sage":
         return discord.Colour.teal()
     
-    elif fake_input == "Phoenix":
+    if fake_input == "Phoenix":
         return 0xEE9346
     
-    elif fake_input == "Jett":
+    if fake_input == "Jett":
         return 0xadd8e6
         
-    elif fake_input == "Reyna":
+    if fake_input == "Reyna":
         return discord.Colour.purple()
     
-    elif fake_input == "Raze":
+    if fake_input == "Raze":
         return discord.Colour.gold()
-    
-    elif fake_input == "Breach":
+        
+    if fake_input == "Breach":
         return discord.Colour.dark_gold()
     
-    elif fake_input == "Skye":
+    if fake_input == "Skye":
         return discord.Colour.green()
+    
+    if fake_input == "Yoru":
+        return 0x393F77
+
+def aliases(fake_input):
+    # Agent codes
+    if fake_input == "Sarge":
+        return "Brimstone"
+    
+    if fake_input == "Pandemic":
+        return "Viper"
+    
+    if fake_input == "Wraith":
+        return "Omen"
+        
+    if fake_input == "Gumshoe":
+        return "Cypher"
+    
+    if fake_input == "Hunter":
+        return "Sova"
+    
+    if fake_input == "Thorne":
+        return "Sage"
+    
+    if fake_input == "Wushu":
+        return "Jett"
+        
+    if fake_input == "Vampire":
+        return "Reyna"
+    
+    if fake_input == "Clay":
+        return "Raze"
+    
+    if fake_input == "Guide":
+        return "Skye"
+
+    # Easter egg aliaes 
+    if fake_input == "Trash":
+        return "Raze"
+
+    if fake_input == "Constant":
+        return "Sova"
+    
+    if fake_input == "Constantvalorant":
+        return "Sova"
+
+    if fake_input == "cabbage":
+        return "Breach"
+
+    if fake_input == "lettuce":
+        return "Breach"
